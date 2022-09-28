2c17eef1fafb4e1c19137c76281a622fef88e9b9
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-09-28 11:05:20 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,13 @@
 class MoviesController < ApplicationController
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+  private
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
   
   def index
     movies = Movie.all
